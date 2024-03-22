0fee856ae9e914f7f9d6d66d7b898a48e23c9153
Yanish <yanishrambocus@gmail.com>
2024-03-22 12:19:06 +0000

    add some more stuff

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    `kotlin-dsl`
+    kotlin("jvm") version "1.9.22"
+    id("com.google.cloud.tools.jib") version "3.4.1"
+    application
+}
+
+group = "io.kerno"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+    implementation("com.google.cloud.tools.jib:com.google.cloud.tools.jib.gradle.plugin:3.3.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+jib {
+    from {
+        image = "eclipse-temurin:17-jre"
+    }
+    to {
+        image = "yanmanatkerno/github-poc"
+        tags = setOf(
+            System.getenv("GITHUB_SHA")
+        )
+    }
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package io.kerno
+
+fun main() {
+    println("Hello World!")
+}
