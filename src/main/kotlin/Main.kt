4de21b20a6cfbafaaccb3babe00e8744bb6fb8ce
Yanish <yanishrambocus@gmail.com>
2024-04-03 09:48:33 +0100

    another print statement :)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,5 @@ fun main() {
     println("Hello World!")
     println("And hello to you Kotlin application")
     println("Going crazy with print statements")
+    println("This is getting ridiculous")
 }
