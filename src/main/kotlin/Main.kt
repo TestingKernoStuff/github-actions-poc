c24d372997051e3f77c725febeb0af9698fadd76
Yanish <yanishrambocus@gmail.com>
2024-04-04 10:03:27 +0100

    new commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,4 +5,5 @@ fun main() {
     println("And hello to you Kotlin application")
     println("Going crazy with print statements")
     println("This is getting ridiculous")
+    println("Mistakes were not learnt from")
 }
